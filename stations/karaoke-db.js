25fc23eaddc1c54f116eea6606285778fc2f73a5
LAULAUtonton <lestevezplasencia@gmail.com>
2026-04-10 22:34:25 +0100

    Create karaoke-db.js

---
+++ stations/karaoke-db.js
@@ -0,0 +1,38 @@
+const karaokeSongs = [
+  {
+    id: "song1",
+    title: "Song 1",
+    artist: "Backing Track",
+    audio: "./audio/karaoke/song1.mp3",
+    lyrics: [
+      "Line 1 of the song",
+      "Line 2 of the song",
+      "Line 3 of the song",
+      "Line 4 of the song"
+    ]
+  },
+  {
+    id: "song2",
+    title: "Song 2",
+    artist: "Backing Track",
+    audio: "./audio/karaoke/song2.mp3",
+    lyrics: [
+      "Line 1 of the song",
+      "Line 2 of the song",
+      "Line 3 of the song",
+      "Line 4 of the song"
+    ]
+  },
+  {
+    id: "song3",
+    title: "Song 3",
+    artist: "Backing Track",
+    audio: "./audio/karaoke/song3.mp3",
+    lyrics: [
+      "Line 1 of the song",
+      "Line 2 of the song",
+      "Line 3 of the song",
+      "Line 4 of the song"
+    ]
+  }
+];
