d0e6813838991b686a12faa91775afbf6fc1af34
LAULAUtonton <lestevezplasencia@gmail.com>
2026-04-11 08:20:26 +0100

    Create twisters-db.js file with tongue twisters data

---
+++ stations/twisters-db.js
@@ -0,0 +1,64 @@
+const tongueTwisters = [
+  {
+    id: "twister1",
+    text: "She sells seashells by the seashore.",
+    audio: "./audio/twisters/she_sells_seashells.mp3",
+    original: "https://www.youtube.com/watch?v=b-KyT4URg8U"
+  },
+  {
+    id: "twister2",
+    text: "How can a clam cram in a clean cream can?",
+    audio: "./audio/twisters/clam_cram.mp3",
+    original: "https://www.youtube.com/watch?v=YBzy4sTsQ-A"
+  },
+  {
+    id: "twister3",
+    text: "I scream, you scream, we all scream for ice cream!",
+    audio: "./audio/twisters/ice_cream.mp3",
+    original: "https://www.youtube.com/watch?v=K9K44fCB-nk"
+  },
+  {
+    id: "twister4",
+    text: "Red lorry, yellow lorry.",
+    audio: "./audio/twisters/red_lorry_yellow_lorry.mp3",
+    original: "https://www.youtube.com/watch?v=fKp8gVl_Nn0"
+  },
+  {
+    id: "twister5",
+    text: "Peter Piper picked a peck of pickled peppers.",
+    audio: "./audio/twisters/peter_piper.mp3",
+    original: "https://www.youtube.com/watch?v=xoVk3fT1tYE"
+  },
+  {
+    id: "twister6",
+    text: "Unique New York.",
+    audio: "./audio/twisters/unique_new_york.mp3",
+    original: "https://www.youtube.com/watch?v=1BFPt001PYg"
+  },
+  {
+    id: "twister7",
+    text: "Fuzzy Wuzzy was a bear. Fuzzy Wuzzy had no hair. Fuzzy Wuzzy wasn’t fuzzy, was he?",
+    audio: "./audio/twisters/fuzzy_wuzzy.mp3",
+    original: "https://www.youtube.com/watch?v=O_xx9vfrAe8"
+  },
+  {
+    id: "twister8",
+    text: "Betty Botter bought some butter, but she said the butter’s bitter.",
+    audio: "./audio/twisters/betty_botter.mp3",
+    original: "https://www.youtube.com/watch?v=NKn3E48lwgI"
+  },
+  {
+    id: "twister9",
+    text: "Can you can a can as a canner can can a can?",
+    audio: "./audio/twisters/can_can.mp3",
+    original: "https://www.youtube.com/watch?v=3TXk55oFfKc"
+  },
+  {
+    id: "twister10",
+    text: "Six sticky skeletons.",
+    audio: "./audio/twisters/six_sticky_skeletons.mp3",
+    original: "https://www.youtube.com/watch?v=4NPXwuTTL-E"
+  }
+];
+
+export default tongueTwisters;
