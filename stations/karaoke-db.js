b7693e1a65a6e5c7054e5234d337a2d448e633bd
LAULAUtonton <lestevezplasencia@gmail.com>
2026-04-11 08:18:22 +0100

    Update karaoke song list in karaoke-db.js

--- stations/karaoke-db.js
+++ stations/karaoke-db.js
@@ -1,38 +1,125 @@
 const karaokeSongs = [
   {
-    id: "song1",
-    title: "Song 1",
-    artist: "Backing Track",
-    audio: "./audio/karaoke/song1.mp3",
+    id: "yellow_submarine",
+    title: "Yellow Submarine",
+    artist: "The Beatles",
+    audio: "./audio/karaoke/yellow_submarine.mp3",
     lyrics: [
-      "Line 1 of the song",
-      "Line 2 of the song",
-      "Line 3 of the song",
-      "Line 4 of the song"
-    ]
+      "In the town where I was born",
+      "Lived a man who sailed the sea",
+      "And he told us of his life",
+      "In the land of submarines"
+    ],
+    original: "https://www.youtube.com/watch?v=m2uTFF_3MaA"
   },
   {
-    id: "song2",
-    title: "Song 2",
-    artist: "Backing Track",
-    audio: "./audio/karaoke/song2.mp3",
+    id: "hello_goodbye",
+    title: "Hello, Goodbye",
+    artist: "The Beatles",
+    audio: "./audio/karaoke/hello_goodbye.mp3",
     lyrics: [
-      "Line 1 of the song",
-      "Line 2 of the song",
-      "Line 3 of the song",
-      "Line 4 of the song"
-    ]
+      "You say yes, I say no",
+      "You say stop and I say go, go, go",
+      "Oh no"
+    ],
+    original: "https://www.youtube.com/watch?v=rblYSKz_VnI"
   },
   {
-    id: "song3",
-    title: "Song 3",
-    artist: "Backing Track",
-    audio: "./audio/karaoke/song3.mp3",
+    id: "stand_by_me",
+    title: "Stand By Me",
+    artist: "Ben E. King",
+    audio: "./audio/karaoke/stand_by_me.mp3",
     lyrics: [
-      "Line 1 of the song",
-      "Line 2 of the song",
-      "Line 3 of the song",
-      "Line 4 of the song"
-    ]
+      "When the night has come",
+      "And the land is dark",
+      "And the moon is the only light we'll see"
+    ],
+    original: "https://www.youtube.com/watch?v=hwZNL7QVJjE"
+  },
+  {
+    id: "lemon_tree",
+    title: "Lemon Tree",
+    artist: "Fools Garden",
+    audio: "./audio/karaoke/lemon_tree.mp3",
+    lyrics: [
+      "I'm sitting here in the boring room",
+      "It's just another rainy Sunday afternoon",
+      "I'm wasting my time",
+      "I got nothing to do"
+    ],
+    original: "https://www.youtube.com/watch?v=sSYU_Jfd_zg"
+  },
+  {
+    id: "count_on_me",
+    title: "Count on Me",
+    artist: "Bruno Mars",
+    audio: "./audio/karaoke/count_on_me.mp3",
+    lyrics: [
+      "If you ever find yourself stuck in the middle of the sea",
+      "I'll sail the world to find you",
+      "If you ever find yourself lost in the dark",
+      "And you can't see"
+    ],
+    original: "https://www.youtube.com/watch?v=ZM2KsaEDOsU"
+  },
+  {
+    id: "i_m_a_believer",
+    title: "I'm a Believer",
+    artist: "The Monkees",
+    audio: "./audio/karaoke/i_m_a_believer.mp3",
+    lyrics: [
+      "Then I saw her face",
+      "Now I'm a believer",
+      "Not a trace of doubt in my mind"
+    ],
+    original: "https://www.youtube.com/watch?v=7r3K1k1EYPg"
+  },
+  {
+    id: "let_it_go",
+    title: "Let It Go",
+    artist: "Idina Menzel",
+    audio: "./audio/karaoke/let_it_go.mp3",
+    lyrics: [
+      "Let it go, let it go",
+      "Can't hold it back anymore",
+      "Let it go, let it go",
+      "Turn away and slam the door"
+    ],
+    original: "https://www.youtube.com/watch?v=L0MK7qz13bU"
+  },
+  {
+    id: "walking_on_sunshine",
+    title: "Walking on Sunshine",
+    artist: "Katrina & The Waves",
+    audio: "./audio/karaoke/walking_on_sunshine.mp3",
+    lyrics: [
+      "I'm walking on sunshine",
+      "Whoa",
+      "And don't it feel good?"
+    ],
+    original: "https://www.youtube.com/watch?v=iPUmE-tne5U"
+  },
+  {
+    id: "let_her_go",
+    title: "Let Her Go",
+    artist: "Passenger",
+    audio: "./audio/karaoke/let_her_go.mp3",
+    lyrics: [
+      "Well you only need the light when it's burning low",
+      "Only miss the sun when it starts to snow",
+      "Only know you love her when you let her go"
+    ],
+    original: "https://www.youtube.com/watch?v=RBumgq5yVrA"
+  },
+  {
+    id: "octopus_garden",
+    title: "Octopus's Garden",
+    artist: "The Beatles",
+    audio: "./audio/karaoke/octopus_garden.mp3",
+    lyrics: [
+      "I'd like to be under the sea",
+      "In an octopus's garden in the shade"
+    ],
+    original: "https://www.youtube.com/watch?v=CUFcfXgW_dQ"
   }
 ];
